d434b7625bbf64b2faae6c1e19c0d87dc7dd73c3
degaga <degagawolde@gmail.com>
2026-05-04 14:46:12 +0300

    fs

--- genai-fundamentals/vector_retriever.py
+++ genai-fundamentals/vector_retriever.py
@@ -4,6 +4,9 @@ load_dotenv()
 
 from neo4j import GraphDatabase
 
+from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
+from neo4j_graphrag.retrievers import VectorRetriever
+
 # Connect to Neo4j database
 driver = GraphDatabase.driver(
     os.getenv("NEO4J_URI"), 
@@ -15,11 +18,24 @@ driver = GraphDatabase.driver(
 
 # Create embedder
 
-# Create retriever
 
+# Create embedder
+embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+# Create retriever
+retriever = VectorRetriever(
+    driver,
+    neo4j_database=os.getenv("NEO4J_DATABASE"),
+    index_name="moviePlots",
+    embedder=embedder,
+    return_properties=["title", "plot"],
+)
 # Search for similar items
+result = retriever.search(query_text="Toys coming alive", top_k=5)
 
 # Parse results
-
+for item in result.items:
+    print(item.content, item.metadata["score"])
+    
 # CLose the database connection
 driver.close()
