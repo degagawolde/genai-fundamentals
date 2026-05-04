3612da11cf389e1f930807e0f4087f68f3dbc48f
degaga <degagawolde@gmail.com>
2026-05-04 14:49:59 +0300

    fs

--- genai-fundamentals/vector_cypher_rag.py
+++ genai-fundamentals/vector_cypher_rag.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from neo4j import GraphDatabase
-from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
 from neo4j_graphrag.llm import OpenAILLM
 from neo4j_graphrag.generation import GraphRAG
+from neo4j_graphrag.retrievers import Text2CypherRetriever
 
 # Connect to Neo4j database
 driver = GraphDatabase.driver(
@@ -16,32 +16,63 @@ driver = GraphDatabase.driver(
     )
 )
 
-# Create embedder
-embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
-
-# Define retrieval query
-retrieval_query =
-
-# Create retriever
-retriever = 
-
-#  Create the LLM
-llm = OpenAILLM(model_name="gpt-5.2")
-
-# Create GraphRAG pipeline
-rag = GraphRAG(retriever=retriever, llm=llm)
-
-# Search
-query_text = "Find the highest rated action movie about travelling to other planets"
-
-response = rag.search(
-    query_text=query_text, 
-    retriever_config={"top_k": 5},
-    return_context=True
+# Create Cypher LLM 
+t2c_llm=OpenAILLM(
+    model_name="gpt-5-mini",
+    model_params={
+        "reasoning_effort": "high"
+    }
 )
 
+# Specify your own Neo4j schema
+neo4j_schema = """
+Node properties:
+Person {name: STRING, born: INTEGER}
+Movie {tagline: STRING, title: STRING, released: INTEGER}
+Genre {name: STRING}
+User {name: STRING}
+
+Relationship properties:
+ACTED_IN {role: STRING}
+RATED {rating: INTEGER}
+
+The relationships:
+(:Person)-[:ACTED_IN]->(:Movie)
+(:Person)-[:DIRECTED]->(:Movie)
+(:User)-[:RATED]->(:Movie)
+(:Movie)-[:IN_GENRE]->(:Genre)
+"""
+
+# Cypher examples as input/query pairs
+examples = [
+    "USER INPUT: 'Get user ratings for a movie?' QUERY: MATCH (u:User)-[r:RATED]->(m:Movie) WHERE m.title = 'Movie Title' RETURN r.rating"
+]
+
+# Build the retriever
+retriever = Text2CypherRetriever(
+    driver=driver,
+    neo4j_database=os.getenv("NEO4J_DATABASE"),
+    llm=t2c_llm,
+    neo4j_schema=neo4j_schema,
+    examples=examples,
+)
+
+llm = OpenAILLM(model_name="gpt-5.2")
+rag = GraphRAG(retriever=retriever, llm=llm)
+
+query_text = "Which movies did Hugo Weaving star in?"
+query_text = "How many movies are in the Sci-Fi genre?"
+query_text = "What is the highest rating for Goodfellas?"
+query_text = "What is the averaging user rating for the movie Toy Story?"
+query_text = "What year was the movie Babe released?"
+
+response = rag.search(
+    query_text=query_text,
+    return_context=True
+    )
+
 print(response.answer)
+print("CYPHER :", response.retriever_result.metadata["cypher"])
 print("CONTEXT:", response.retriever_result.items)
 
-# Close the database connection
 driver.close()
